f6d54073f1428dd7ed4d8cf874860c137d201dcb
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-08-16 14:18:27 +0530

    Package Introduce Methods

---
+++ src/com/jagdambha/init/DefaultValues.java
@@ -0,0 +1,14 @@
+package com.jagdambha.init;
+
+public class DefaultValues {
+
+	String city;
+	boolean isActive;
+	public static void main(String[] args) {
+		
+		DefaultValues df = new DefaultValues();
+		System.out.println(df.city);
+		System.out.println(df.isActive);
+	}
+
+}
