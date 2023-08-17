7507d46ebfcd0a2dda91f38bc0c0393c098d1df8
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-08-17 15:38:28 +0530

    Method with arguement

---
+++ src/com/jagdambha/methods/MethodWithArguement.java
@@ -0,0 +1,24 @@
+package com.jagdambha.methods;
+
+public class MethodWithArguement {
+
+	public static void main(String[] args) {
+		
+		//Methods with Arguement
+		getEmployeeId(1060);
+		MethodWithArguement arguement = new MethodWithArguement();
+		arguement.getAllData(23,"Shampoo",1200.99);
+
+	}
+	
+	private void getAllData(int productId, String productName,double price) {
+		// TODO Auto-generated method stub
+		System.out.println(productId+ " "+productName+" "+price);
+		
+	}
+
+	public static void getEmployeeId(int employeeId) {
+		System.out.println("The Employee Id :"+employeeId);
+	}
+
+}
