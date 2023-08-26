38d239c4a6011365acc22ea64709301198354be0
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-08-26 15:35:47 +0530

    Method Overloading and this keyword

--- src/com/jagdambha/casting/Model.java
+++ src/com/jagdambha/casting/Model.java
@@ -6,32 +6,23 @@ public class Model {
 	String fName,mName,lName;
 	long mobile;
 	String address;
-	
-	
-	public Model() {
-		super();
+	public int getId() {
+		return id;
 	}
-
-	public Model(int id, String fName) {
-		super();
-		this.id = id;
-		this.fName = fName;
+	public String getfName() {
+		return fName;
 	}
-	
-	public Model(long mobile, String address) {
-		super();
-		this.mobile = mobile;
-		this.address = address;
+	public String getmName() {
+		return mName;
 	}
-	
-	public Model(int id, String fName, String mName, String lName, long mobile, String address) {
-		super();
-		this.id = id;
-		this.fName = fName;
-		this.mName = mName;
-		this.lName = lName;
-		this.mobile = mobile;
-		this.address = address;
+	public String getlName() {
+		return lName;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public String getAddress() {
+		return address;
 	}
 	
 	
---
+++ src/com/jagdambha/methods/MethodOverloading.java
@@ -0,0 +1,51 @@
+package com.jagdambha.methods;
+
+public class MethodOverloading {
+
+	int id;
+	String fName,lName;
+	long pinCode,mobile;
+	
+	public static void main(String[] args) {
+		
+		MethodOverloading mo = new MethodOverloading();
+		mo.getData();
+		mo.getData(1128);
+		mo.getData("Jagdambha", "College");
+		mo.getData(445002L, 9890581851L);
+		
+		mo.getData(mo.id, mo.fName, mo.lName, mo.pinCode, mo.mobile);
+
+	}
+	
+	public void getData() {
+		System.out.println("Method having no arguement");
+	}
+	
+	public void getData(int id) {
+//		System.out.println("My Employee Id is "+id);
+		this.id = id;
+	}
+	
+	public void getData(String fName, String lName) {
+//		System.out.println("My Name is "+fName + " "+lName);
+		this.fName = fName;
+		this.lName = lName;
+	}
+	
+	public void getData(long pinCode, long mobile) {
+//		System.out.println("Pincode is "+pinCode+ "and Mobile is "+mobile);
+		
+		this.pinCode = pinCode;
+		this.mobile = mobile;
+	}
+	
+	public void getData(int id,String fName,String lName,long pinCode,long mobile) {
+		System.out.println(id);
+		System.out.println(fName);
+		System.out.println(lName);
+		System.out.println(pinCode);
+		System.out.println(mobile);
+	}
+
+}
