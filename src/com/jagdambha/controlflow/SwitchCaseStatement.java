5cbdc397a2428dcfac19fbc90306ab54400f477b
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-09-02 13:27:02 +0530

    Switch Case

--- src/com/jagdambha/controlflow/SwitchCaseStatement.java
+++ src/com/jagdambha/controlflow/SwitchCaseStatement.java
@@ -9,31 +9,36 @@ public class SwitchCaseStatement {
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("1. Addition \n 2.Substraction \n 3.Division \n 4.Multiplication \n 5.Exit \n Enter your choice…………");
-		int x=Integer.parseInt(br.readLine());
+		System.out.println("Your Choices Are \n1.Addition \n2.Substraction \n3.Division "
+				+ "\n4.Multiplication \n5.Exit\n Enter your choice…………");
+		
+		int choice =Integer.parseInt(br.readLine());
+		
 		System.out.println("Enter num1");
-		int a=Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine());
+		
 		System.out.println("Enter num2");
-		int b=Integer.parseInt(br.readLine());
-		int c=0;
+		int b = Integer.parseInt(br.readLine());
+		
+		int c = 0;
 
-		switch(x)
+		switch(choice)
 		{	
-			case 1:c=a+b;
+			case 1: c=a+b;
 				System.out.println("Addition of "+a+"and"+b+"="+c);
 				break;
-			case 2:c=a-b;
+			case 2: c=a-b;
 				System.out.println("Substraction of "+a+"and"+b+"="+c);
 				break;
-			case 3:c=a*b;
-				System.out.println("Multiplication of "+a+"and"+b+"="+c);
+			case 3: c=a*b;
+				System.out.println("Multiplication of "+a+" and "+b+" = "+c);
 				break;
-			case 4:c=a/b;
+			case 4: c=a/b;
 				System.out.println("Division of "+a+"and"+b+"="+c);
 				break;
-			case 5:System.exit(0);
+			case 5: System.exit(0);
 				break;
-			default:System.out.println("Invalid choice");
+			default: System.out.println("Invalid choice");
 		}
 
 
