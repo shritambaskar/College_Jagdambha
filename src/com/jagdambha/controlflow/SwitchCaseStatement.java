c82919da6e9a9cf862e33918329d654afc400236
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-09-02 13:51:13 +0530

    Switch with while loop

--- src/com/jagdambha/controlflow/SwitchCaseStatement.java
+++ src/com/jagdambha/controlflow/SwitchCaseStatement.java
@@ -8,38 +8,46 @@ public class SwitchCaseStatement {
 
 	public static void main(String[] args) throws Exception {
 		
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Your Choices Are \n1.Addition \n2.Substraction \n3.Division "
-				+ "\n4.Multiplication \n5.Exit\n Enter your choice…………");
+		char ch = 'y';
 		
-		int choice =Integer.parseInt(br.readLine());
-		
-		System.out.println("Enter num1");
-		int a = Integer.parseInt(br.readLine());
-		
-		System.out.println("Enter num2");
-		int b = Integer.parseInt(br.readLine());
-		
-		int c = 0;
+		while (ch == 'y' || ch == 'Y') {
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Your Choices Are \n1.Addition \n2.Substraction \n3.Division "
+					+ "\n4.Multiplication \n5.Exit\n Enter your choice…………");
+			
+			int choice =Integer.parseInt(br.readLine());
+			
+			System.out.println("Enter num1");
+			int a = Integer.parseInt(br.readLine());
+			
+			System.out.println("Enter num2");
+			int b = Integer.parseInt(br.readLine());
+			
+			int c = 0;
 
-		switch(choice)
-		{	
-			case 1: c=a+b;
-				System.out.println("Addition of "+a+"and"+b+"="+c);
-				break;
-			case 2: c=a-b;
-				System.out.println("Substraction of "+a+"and"+b+"="+c);
-				break;
-			case 3: c=a*b;
-				System.out.println("Multiplication of "+a+" and "+b+" = "+c);
-				break;
-			case 4: c=a/b;
-				System.out.println("Division of "+a+"and"+b+"="+c);
-				break;
-			case 5: System.exit(0);
-				break;
-			default: System.out.println("Invalid choice");
+			switch(choice)
+			{	
+				case 1: c=a+b;
+					System.out.println("Addition of "+a+"and"+b+"="+c);
+					break;
+				case 2: c=a-b;
+					System.out.println("Substraction of "+a+"and"+b+"="+c);
+					break;
+				case 3: c=a*b;
+					System.out.println("Multiplication of "+a+" and "+b+" = "+c);
+					break;
+				case 4: c=a/b;
+					System.out.println("Division of "+a+"and"+b+"="+c);
+					break;
+				case 5: System.exit(0);
+					break;
+				default: System.out.println("Invalid choice");
+			}
+			
+			System.out.println("Do You Wish to Continue Y/N");
+			ch = (char) br.read();
 		}
+		
 
 
 	}
---
+++ src/com/jagdambha/controlflow/WhileLoopTest.java
@@ -0,0 +1,17 @@
+package com.jagdambha.controlflow;
+
+public class WhileLoopTest {
+
+	public static void main(String[] args) {
+		//Initialisation of Counter
+		int counter = 0;
+		
+		//Condition using while loop
+		while (counter < 5) {
+			System.out.println(counter);
+			counter++;
+		}
+
+	}
+
+}
