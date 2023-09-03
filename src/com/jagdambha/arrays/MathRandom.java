9bffc39ae8833d9ee5474d76f0418de392c10c42
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-09-03 15:23:35 +0530

    Math Class

---
+++ src/com/jagdambha/arrays/MathRandom.java
@@ -0,0 +1,16 @@
+package com.jagdambha.arrays;
+
+public class MathRandom {
+
+	public static void main(String[] args) {
+//		int otp =  (int) Math.ceil(Math.random() * 1000000);
+//		
+//		System.out.println("Your OTP is :"+otp);
+		
+		System.out.println(Math.ceil(Math.random()));
+		
+		double d = 23.45;
+		System.out.println(Math.ceil(d));
+		System.out.println(Math.floor(d));
+	}
+}
