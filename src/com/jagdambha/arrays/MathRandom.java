6275770035d40664442dc6acbeb62929a3c975b7
Shri-Ecsion <shri.tambaskar@gmail.com>
2023-09-03 15:33:25 +0530

    OTP Login

--- src/com/jagdambha/arrays/MathRandom.java
+++ src/com/jagdambha/arrays/MathRandom.java
@@ -1,12 +1,24 @@
 package com.jagdambha.arrays;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MathRandom {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		int otp =  (int) Math.ceil(Math.random() * 1000000);
 		
 		System.out.println("Your OTP is :"+otp);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
+		System.out.println("Enter OTP");
+		int inputOTP = Integer.parseInt(br.readLine());
+		
+		if(otp == inputOTP) {
+			System.out.println("Login Successfully...");
+		}else {
+			System.out.println("Incorrect OTP");
+		}
 //		System.out.println(Math.ceil(Math.random()));
 //		
 //		double d = 23.45;
